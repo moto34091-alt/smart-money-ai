3d432beb58ed0e52f3d13c16fdb1207b613e2de2
Support Vimba boss <moto34091@gmail.com>
2026-05-22 01:02:52 +0200

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,21 @@
+package com.smartmoneyai
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://ton-site.com")
+
+        setContentView(webView)
+    }
+}
